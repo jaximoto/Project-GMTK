dacc25ed8bc3a7e49df5bd666696f2e58de54d85
Hooninator <jbellavita@berkeley.edu>
2025-07-30 19:53:19 -0400

    movement

--- Assets/Scripts/WheelMovement.cs
+++ Assets/Scripts/WheelMovement.cs
@@ -2,11 +2,20 @@ using UnityEngine;
 
 public class WheelMovement : MonoBehaviour
 {
+
+    [SerializeField] public float torqueAmount = 10f;
+    [SerializeField] public float decelerationRate = 1.0f;
+    [SerializeField] public float breakAmplifier = 2.0f;
+    private Rigidbody2D rb;
+    private bool applyInput;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        rb = GetComponent<Rigidbody2D>();
     }
+
 
     // Update is called once per frame
     void Update()
@@ -15,13 +24,43 @@ public class WheelMovement : MonoBehaviour
     }
 
 
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.S))
+        {
+            applyInput = true;
+            Break();
+            Debug.Log("Breaking");
+        }
+
+        if (!applyInput)
+        {
+            rb.AddTorque(-rb.angularVelocity * decelerationRate * Time.deltaTime);
+        }
+    }
+
+
+    void Break()
+    {
+        //rb.angularVelocity = Mathf.MoveTowards(rb.angularVelocity, 0f, breakAmplifier * Time.deltaTime);
+        rb.angularVelocity = 0f;
+        rb.linearVelocity = Vector2.zero;
+    }
+
+
     void HandleMovement()
     {
-
-        float horiz = Input.GetAxisRaw("Horizontal"); 
-        float vert = Input.GetAxisRaw("Vertical"); 
-
-        
-
+        applyInput = false;
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddTorque(torqueAmount * Time.deltaTime);
+            applyInput = true;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddTorque(-torqueAmount * Time.deltaTime);
+            applyInput = true;
+        }
     }
+
 }
