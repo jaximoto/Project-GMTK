417a2c4e2528e89c8c0eef5a4ed7e6d4fb40e812
jaximoto <jaximods@gmail.com>
2025-08-01 10:39:59 -0700

    jesus christ

--- Assets/Scripts/Visual Player.cs
+++ Assets/Scripts/Visual Player.cs
@@ -3,11 +3,15 @@ using UnityEngine;
 public class VisualPlayer : MonoBehaviour
 {
     public WheelMovement _playerMovement;
-    
-   
+    public Rigidbody2D _playerRigidbody;
+    Vector2 _tmp;
+    Vector3 _localScale;
+    Vector3 _flippedLocalScale;
     void Awake()
     {
         _playerMovement = FindFirstObjectByType<WheelMovement>();
+        _localScale = transform.localScale;
+        _flippedLocalScale = new Vector3(transform.localScale.x * -1, _localScale.y);
         
     }
 
@@ -15,5 +19,23 @@ public class VisualPlayer : MonoBehaviour
     void Update()
     {
         transform.up = _playerMovement.GetNormal();
+        
+        if (_playerRigidbody.angularVelocity > 0)
+        {
+            Debug.Log("In if");
+            /*
+            _tmp = Vector2.Perpendicular(_playerMovement.GetNormal());
+            transform.right = new Vector2(-_tmp.x, _tmp.y);
+            */
+            transform.localScale = _flippedLocalScale;
+
+        }
+
+        else
+        {
+            //transform.right = -Vector2.Perpendicular(_playerMovement.GetNormal());
+            transform.localScale = _localScale;
+        }
+        
     }
 }
